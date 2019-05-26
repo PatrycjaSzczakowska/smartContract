f20af2827be0761233502e856a4f4f068a9f6aa8
Patrycja Szczakowska <patrycja.szczakowska@gmail.com>
2019-05-26 22:16:16 +0200

    Messages build process changed

--- src/main/java/org/hyperledger/fabric/chaincode/ChaincodeResponse.java
+++ src/main/java/org/hyperledger/fabric/chaincode/ChaincodeResponse.java
@@ -1,41 +1,30 @@
 package org.hyperledger.fabric.chaincode;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
 
 public class ChaincodeResponse {
-    public String message;
-    public String code;
-    public boolean OK;
-
-    public ChaincodeResponse(String message, String code, boolean OK) {
-        this.message = message;
-        this.code = code;
-        this.OK = OK;
-    }
 
     public static String responseError(String errorMessage) {
-        try {
-            return (new ObjectMapper()).writeValueAsString(new ChaincodeResponse(errorMessage, "", false));
-        } catch (Throwable e) {
-            return "{\"message\":'" + e.getMessage() + " AND " + errorMessage + "', \"OK\":" + false + "}";
-        }
+        return response(errorMessage,false);
+
     }
 
     public static String responseSuccess(String successMessage) {
-        try {
-            return (new ObjectMapper()).writeValueAsString(new ChaincodeResponse(successMessage, "", true));
-        } catch (Throwable e) {
-            return "{\"message\":'" + e.getMessage() + " BUT " + successMessage + " (NO COMMIT)', \"OK\":" + false + "}";
-        }
+            return response(successMessage,true);
     }
+
+    private static String response(String successMessage, boolean ok) {
+        JsonObject obj = new JsonObject();
+        obj.addProperty("message", successMessage);
+        obj.addProperty("OK", ok);
+        return obj.getAsString();
+    }
+
 
     public static String responseSuccessObject(String object) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("{\"message\":");
-        stringBuilder.append(object);
-        return stringBuilder.toString().replaceAll("\\\\","");
+        JsonObject obj = new JsonObject();
+        obj.addProperty("message", object);
+        return obj.getAsString();
     }
-
-
 }
 
