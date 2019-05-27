a13c30dff5d71c136c6c1e463b091564815d6278
Patrycja Szczakowska <patrycja.szczakowska@gmail.com>
2019-05-27 13:00:25 +0200

    Messages build process changed

--- src/main/java/org/hyperledger/fabric/chaincode/ChaincodeResponse.java
+++ src/main/java/org/hyperledger/fabric/chaincode/ChaincodeResponse.java
@@ -3,35 +3,36 @@ package org.hyperledger.fabric.chaincode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ChaincodeResponse {
-    String message;
-    String status;
+    public String message;
+    public String code;
+    public boolean OK;
 
-    public ChaincodeResponse(String message, String status) {
+    public ChaincodeResponse(String message, String code, boolean OK) {
         this.message = message;
-        this.status = status;
+        this.code = code;
+        this.OK = OK;
     }
 
     public static String responseError(String errorMessage) {
         try {
-            return (new ObjectMapper()).writeValueAsString(new ChaincodeResponse(errorMessage,  "ERROR"));
+            return (new ObjectMapper()).writeValueAsString(new ChaincodeResponse(errorMessage, "", false));
         } catch (Throwable e) {
-            return "{\"message\":'" + e.getMessage() + " AND " + errorMessage + "', \"OK\":" + false + "}";
+            return "{\"code\":'" + "" + "', \"message\":'" + e.getMessage() + " AND " + errorMessage + "', \"OK\":" + false + "}";
         }
     }
 
     public static String responseSuccess(String successMessage) {
         try {
-            return (new ObjectMapper()).writeValueAsString(new ChaincodeResponse(successMessage, "OK"));
+            return (new ObjectMapper()).writeValueAsString(new ChaincodeResponse(successMessage, "", true));
         } catch (Throwable e) {
             return "{\"message\":'" + e.getMessage() + " BUT " + successMessage + " (NO COMMIT)', \"OK\":" + false + "}";
         }
     }
 
     public static String responseSuccessObject(String object) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("{\"message\":");
-        stringBuilder.append(object);
-        return stringBuilder.toString();
+        return "{\"message\":" + object + ", \"OK\":" + true + "}";
     }
 }
+
+
 
